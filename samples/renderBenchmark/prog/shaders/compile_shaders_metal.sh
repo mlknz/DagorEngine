309a4e9dcbc3361368e02a00263806e28b02a549
Ramil Kudashev <ramil.kudashev.ext@astrum.team>
2024-04-15 12:05:10 +0300

    Create renderBenchmark sample by copying testGI sample

---
+++ samples/renderBenchmark/prog/shaders/compile_shaders_metal.sh
@@ -0,0 +1,1 @@
+../../../../tools/dagor3_cdk/util-macosx/dsc2-metal-dev ./shaders_metal.blk -q -shaderOn -nodisassembly -commentPP -codeDumpErr -bones_start 70  -o ../../../../_output/shaders/testGI-game~metal
